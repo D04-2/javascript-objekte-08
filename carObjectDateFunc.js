003511ceca1bf1c1640dedbda6a9d8b4981bab49
MrGultekin <gultekin.de@gmail.com>
2022-02-17 11:58:32 +0100

    Done

---
+++ carObjectDateFunc.js
@@ -0,0 +1,15 @@
+const muellerCar = {
+    brand: 'BMW',
+    model: 'X5',
+    color: 'blue',
+    mileage: 40000,
+    year: 2019,
+    today: new Date().getFullYear(),
+    age: function () {
+        return  this.today -this.year;
+    }
+}
+console.log(muellerCar.age())
+
+
+
